1cd602fefb33e454709a3e41d696ad12a6248961
ineskab <ineskab@users.noreply.github.com>
2019-12-06 15:43:53 +0000

    automated commit by check50 [check50=True]

--- mario.py
+++ mario.py
@@ -3,9 +3,9 @@ from cs50 import get_int
 # Prompt user for a positive number
 while True:
     n = get_int("Write the number between 1 and 8: ")
-    if n >= 0 and n <= 8:
+    if n > 0 and n <= 8:
         break
 
 for i in range(n):
     print("#"*(i + 1))
-
+print()
