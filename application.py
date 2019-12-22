58417bd7a6685869e078b269891e0197cf621d70
ineskab <ineskab@users.noreply.github.com>
2019-12-22 19:53:52 +0000

    automated commit by check50 [check50=True]

--- application.py
+++ application.py
@@ -26,6 +26,7 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+
 # Custom filter
 app.jinja_env.filters["usd"] = usd
 
@@ -49,8 +50,8 @@ def index():
     """Show portfolio of stocks"""
 
     stocks = db.execute("SELECT symbol, SUM(shares) as shares FROM history WHERE id=:id GROUP BY symbol",
-        id = session["user_id"])
-    cash = db.execute("SELECT cash FROM users WHERE id =:id", id = session["user_id"])
+                        id=session["user_id"])
+    cash = db.execute("SELECT cash FROM users WHERE id =:id", id=session["user_id"])
     cash = cash[0]["cash"]
 
     total = 0
@@ -63,9 +64,9 @@ def index():
         stock["total"] = '{:.2f}'.format(stock["total"])
     total += cash
     return render_template("index.html",
-        stocks=stocks,
-        cash='{:.2f}'.format(cash),
-        total='{:.2f}'.format(total))
+                           stocks=stocks,
+                           cash='{:.2f}'.format(cash),
+                           total='{:.2f}'.format(total))
 
 
 def is_string_int(s):
@@ -104,7 +105,7 @@ def buy():
             return apology("number of shares has to be a positive number", 400)
 
         # Check if the user can afford
-        cash = db.execute("SELECT cash FROM users WHERE id =:id", id = session["user_id"])
+        cash = db.execute("SELECT cash FROM users WHERE id =:id", id=session["user_id"])
         cash = cash[0]["cash"]
 
         price = quote["price"]
@@ -113,19 +114,19 @@ def buy():
             return apology("you can't afford ")
         # if alright
         # update the cash in users table
-        db.execute("UPDATE users SET cash =:current_cash WHERE id=:id", id = session["user_id"], current_cash = current_cash)
+        db.execute("UPDATE users SET cash =:current_cash WHERE id=:id", id=session["user_id"], current_cash=current_cash)
         # update the portfolio
-        rows = db.execute("SELECT * FROM portfolio WHERE id=:id AND symbol=:symbol", id = session["user_id"], symbol=symbol)
+        rows = db.execute("SELECT * FROM portfolio WHERE id=:id AND symbol=:symbol", id=session["user_id"], symbol=symbol)
         if len(rows) == 0:
             db.execute("INSERT INTO portfolio (id, symbol, shares) VALUES(:id, :symbol, :shares)",
-                id=session["user_id"], symbol=symbol, shares=shares)
+                       id=session["user_id"], symbol=symbol, shares=shares)
         else:
-            db.execute("UPDATE portfolio SET shares = shares + :shares WHERE id =:id", shares=shares, id = session["user_id"])
+            db.execute("UPDATE portfolio SET shares = shares + :shares WHERE id =:id", shares=shares, id=session["user_id"])
 
         # update the history table
         transacted = datetime.now()
         db.execute("INSERT INTO history (id, symbol, shares, price, transacted) VALUES (:id, :symbol, :shares, :price, :transacted)",
-            id=session["user_id"], symbol = symbol, shares = shares, price=price, transacted=transacted )
+                   id=session["user_id"], symbol=symbol, shares=shares, price=price, transacted=transacted)
 
         return redirect("/")
 
@@ -136,9 +137,9 @@ def buy():
 @app.route('/<username>', methods=["GET"])
 def check(username):
     """Return true if username available, else false, in JSON format"""
-    username = request.args.get('username', type = str)
+    username = request.args.get('username', type=str)
     rows = db.execute("SELECT * FROM users WHERE username = :username",
-        username=username)
+                      username=username)
     if rows:
         return jsonify(False)
     else:
@@ -150,7 +151,7 @@ def check(username):
 def history():
     """Show history of transactions"""
     histories = db.execute("SELECT *  FROM history WHERE id=:id",
-    id = session["user_id"])
+                           id=session["user_id"])
 
     return render_template("history.html", histories=histories)
 
@@ -215,20 +216,18 @@ def quote():
 
         symbol = request.form.get("symbol").upper()
         quote = lookup(symbol)
-         # Validate symbol
+        # Validate symbol
         if quote is None:
             return apology("Wrong symbol")
 
         return render_template("quoted.html",
-            symbol=symbol,
-            name=quote["name"],
-            price='{:.2f}'.format(quote["price"]))
-
+                               symbol=symbol,
+                               name=quote["name"],
+                               price='{:.2f}'.format(quote["price"]))
     else:
         return render_template("quote.html")
 
 
- # code Agniesi!!!!!!!------------------------------------------
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
@@ -245,12 +244,10 @@ def register():
         if not request.form.get("password"):
             return apology("must provide password", 400)
 
-
         is_available = json.loads(check(username).response[0])
         #is_available = check(username)
         if not is_available:
             return apology("provided username already exists", 400)
-
 
         # Ensure password was submitted
         elif not request.form.get("confirmation"):
@@ -261,8 +258,8 @@ def register():
 
         # Add user to databse
         result = db.execute("INSERT INTO users (username, hash) VALUES (:username, :hash)",
-            username = username,
-            hash= generate_password_hash(request.form.get("password")))
+                            username=username,
+                            hash=generate_password_hash(request.form.get("password")))
         if not result:
             return apology("rrr")
 
@@ -275,16 +272,13 @@ def register():
         return render_template("register.html")
 
 
-
-# code the end Agniesi!!!!!!!!!!!------------------------
-
 @app.route("/sell", methods=["GET", "POST"])
 @login_required
 def sell():
     """Sell shares of stock"""
     if request.method == "POST":
-    # require that a user input a stock’s symbol
-    # render an apology if the user fails to select a stock
+        # require that a user input a stock’s symbol
+        # render an apology if the user fails to select a stock
 
         if not request.form.get("symbol"):
             return apology("must provide symbol", 400)
@@ -310,41 +304,44 @@ def sell():
 
         # implemented as a select menu whose name is symbol.
         shares_owned = db.execute("SELECT shares FROM portfolio WHERE id =:id and symbol= :symbol",
-            id = session["user_id"], symbol=symbol)
+                                  id=session["user_id"],
+                                  symbol=symbol)
         # render an apology  if the user does not own any shares of that stock.
         if len(shares_owned) == 0:
             return apology("You don't owned this stock", 400)
 
         shares_owned = shares_owned[0]["shares"]
-        shares_update= shares_owned  - shares
+        shares_update = shares_owned - shares
 
         if shares > shares_owned:
             return apology("To much", 400)
 
-
         cash_update = shares * price
 
         # Update the cash user table- dobrze
-        db.execute("UPDATE users SET cash = cash + :cash_update WHERE id =:id", cash_update=cash_update, id = session["user_id"])
+        db.execute("UPDATE users SET cash = cash + :cash_update WHERE id =:id",
+                   cash_update=cash_update, id=session["user_id"])
 
         # Update portfolio table
         if shares_update == 0:
 
-            db.execute("DELETE from portfolio WHERE id =:id AND symbol=:symbol", id = session["user_id"], symbol=symbol)
+            db.execute("DELETE from portfolio WHERE id =:id AND symbol=:symbol", id=session["user_id"], symbol=symbol)
 
         elif shares_update > 0:
-            db.execute("UPDATE portfolio SET shares = shares + :shares_update WHERE id =:id", shares_update=shares_update, id = session["user_id"])
+            db.execute("UPDATE portfolio SET shares = shares + :shares_update WHERE id =:id",
+                       shares_update=shares_update, id=session["user_id"])
 
         # Update history table
         transacted = datetime.now()
         db.execute("INSERT INTO history (id, symbol, shares, price, transacted) VALUES (:id, :symbol, :shares, :price, :transacted)",
-            id=session["user_id"], symbol = symbol, shares = -(shares), price=price, transacted=transacted )
+                   id=session["user_id"], symbol=symbol, shares=-(shares), price=price, transacted=transacted)
 
         return redirect("/")
     else:
         stocks = db.execute("SELECT symbol FROM portfolio WHERE id=:id",
-            id = session["user_id"])
+                            id=session["user_id"])
         return render_template("sell.html", stocks=stocks)
+
 
 def errorhandler(e):
     """Handle error"""
@@ -367,7 +364,6 @@ def change_passwords():
 
         # change passwords for user loggedin
 
-
         # Ensure username was submitted
         if not request.form.get("username"):
             return apology("must provide username", 403)
@@ -376,7 +372,7 @@ def change_passwords():
         elif not request.form.get("old_password"):
             return apology("must provide password", 403)
 
-         # Ensure new password was submitted
+        # Ensure new password was submitted
         elif not request.form.get("new_password"):
             return apology("must provide password", 403)
 
@@ -386,14 +382,16 @@ def change_passwords():
 
         # Ensure passwords match
         elif request.form.get("new_password_confirmation") != request.form.get("new_password"):
-                return apology("Password doesn't match!", 403)
+            return apology("Password doesn't match!", 403)
 
-        current_hash = db.execute("SELECT hash FROM users WHERE id =:id", id = session["user_id"])
+        current_hash = db.execute("SELECT hash FROM users WHERE id =:id",
+                                  id=session["user_id"])
         if check_password_hash(current_hash[0]['hash'],
-            generate_password_hash(request.form.get("old_password"))):
+                               generate_password_hash(request.form.get("old_password"))):
             return apology("old password is wrong", 403)
 
-        db.execute("UPDATE users SET hash =:hash WHERE id=:id", id = session["user_id"], hash = generate_password_hash(request.form.get("new_password")))
+        db.execute("UPDATE users SET hash =:hash WHERE id=:id",
+                   id=session["user_id"], hash=generate_password_hash(request.form.get("new_password")))
         flash("password changed")
         return redirect("/")
 
